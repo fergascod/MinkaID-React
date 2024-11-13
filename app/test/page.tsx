75d2c43149c762bd3cfcf2ead96d33ac751e564a
Fernando Gastón <fergascod@gmail.com>
2024-11-13 18:56:09 +0100

    Working test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,4 +1,9 @@
+"use client"
+
 import game_modes from '../game_modes.json'
+
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation'
 
 function get_game_modes() {
     console.log(game_modes)
@@ -10,7 +15,7 @@ function get_game_modes() {
     return options
 }
 
-export default function Test() {
+function TestForm() {
     get_game_modes()
     return (
         <div className="w-full max-w-xs text-center flex justify-center ">
@@ -44,4 +49,130 @@ export default function Test() {
             </form>
         </div>
     );
+}
+
+function returnName(sp) {
+    if ("preferred_common_name" in sp) {
+        return `${sp["preferred_common_name"]} (${sp["name"]})`
+    }
+    return sp["name"]
+}
+
+function Question({ question, onGenerateNewQuestion, setResp, updateScore }) {
+    const options = question.species.map((species, i) => (
+        <div key={i}>
+            <button onClick={() => { setResp(i); console.log(question["correct"] == i); updateScore(question["correct"] == i); onGenerateNewQuestion(); }}>
+                {returnName(species)}
+            </button>
+            <br />
+        </div>
+    ));
+
+    return (
+        <div>
+            <img src={question.url}></img>
+            <ul>{options}</ul>
+            <button onClick={onGenerateNewQuestion}>Next Question</button>
+        </div>
+    )
+}
+
+function getRandomCombination(arr, k) {
+    let tempArr = [...arr];
+    let combination = [];
+    for (let i = 0; i < k; i++) {
+        const randIndex = Math.floor(Math.random() * tempArr.length);
+        combination.push(tempArr[randIndex]);
+        tempArr.splice(randIndex, 1);
+    }
+    return combination;
+}
+
+export default function Test() {
+    const searchParams = useSearchParams()
+    const taxon_id = searchParams.get('taxon_id')
+    const num_questions = parseInt(searchParams.get('num_questions'))
+
+    if (!taxon_id) return TestForm();
+
+    const [taxonId, setTaxonId] = useState(null);
+    const [numQuestions, setNumQuestions] = useState(null);
+    const [ans, setAns] = useState<number | null>(null);
+    const [data, setData] = useState(null);
+    const [question, setQuestion] = useState(null);
+    const [resp, setResp] = useState(null);
+    const [points, setPoints] = useState(0);
+
+    // Set up taxonId and numQuestions from router query parameters
+    useEffect(() => {
+        if (taxon_id) { // Ensure query param is loaded before setting state
+            setTaxonId(/^\+?(0|[1-9]\d*)$/.test(taxon_id) ? taxon_id : "1");
+            setNumQuestions(/^\+?(0|[1-9]\d*)$/.test(num_questions) ? num_questions : "1");
+        }
+    }, [taxon_id, num_questions]);
+
+    // Fetch species data based on taxonId
+    useEffect(() => {
+        if (taxonId) {
+            const apiUrl = `https://minka-sdg.org:4000/v1/taxa?taxon_id=${taxonId}&rank=species&locale=ca&per_page=10`;
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(json => setData(json))
+                .catch(error => console.error(error));
+        }
+    }, [taxonId]);
+
+    // Function to generate a new question
+    const generateQuestion = () => {
+        if (data) {
+            if (ans !== null) setAns(ans + 1);
+            else setAns(0);
+            const species = data["results"];
+            const numOptions = 5;
+            const options = getRandomCombination(species, numOptions);
+            const correctIndx = Math.floor(Math.random() * numOptions);
+            const apiUrl = `https://minka-sdg.org:4000/v1/observations?photo_license=cc-by-nc&user_id=xasalva&taxon_id=${options[correctIndx]["id"]}&quality_grade=research&order=desc&order_by=created_at`;
+
+            fetch(apiUrl)
+                .then(response => response.json())
+                .then(json => setQuestion({
+                    url: json["results"][0]["photos"][0]["url"],
+                    species: options,
+                    correct: correctIndx
+                }))
+                .catch(error => console.error(error));
+        }
+    };
+
+    // Call generateQuestion initially when data becomes available
+    useEffect(() => {
+        if (data) {
+            generateQuestion();
+        }
+    }, [data]);
+
+    useEffect(() => {
+        console.log(question)
+        console.log(points)
+        console.log(ans)
+    }, [question])
+
+    if (ans < num_questions) {
+        return (
+            <div>
+                {question
+                    ? <Question question={question}
+                        onGenerateNewQuestion={generateQuestion}
+                        setResp={setResp}
+                        updateScore={(b: boolean) => { setPoints(points + (b ? 1 : 0)) }} />
+                    : "Loading..."}
+                <button onClick={() => console.log(resp)}>check</button>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <p>{`${points}/${num_questions}`}</p>
+        </div>
+    )
 }
